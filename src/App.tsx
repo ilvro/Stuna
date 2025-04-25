6982289f57d0aa2d475fde3459443e8ecbb40437
ilvro <cildumyt@gmail.com>
2025-04-24 21:08:52 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <Title text="Hello World!"></Title>
-  )
+    return (
+        <div>
+            <Title text="Hello World!"></Title>
+            <CountButton></CountButton>
+        </div>
+    )
 }
 
 interface TitleProps {
     text: string;
 }
+
+interface CountProps {
+    initialCount?: number;
+}
+
 function Title(props: TitleProps) {
     return <h1>{props.text}</h1>
 }
 
-export default App
+function CountButton({initialCount = 0}: CountProps) {
+    const [count, setCount] = useState(initialCount);
+    const handleClick = () => {
+        setCount(count+1);
+    }
+
+    return (
+        <button onClick={handleClick}>
+            {count}
+        </button>
+    )
+}
+
+
+export default App;
