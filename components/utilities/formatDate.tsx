3f6e894cf4449e298934cd84e48a2198a8fd516b
ilvro <cildumyt@gmail.com>
2025-05-23 16:05:00 -0300

    added today/yesterday check for dates on question cards

--- components/utilities/formatDate.tsx
+++ components/utilities/formatDate.tsx
@@ -1,10 +1,27 @@
-export function formatDate(dateString: string): string {
-    const date = new Date(dateString);
-    return date.toLocaleString('pt-BR', {
+function getTime(dateString: Date) {
+    return dateString.toLocaleString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+    })
+}
+
+function getDate(dateString: Date) {
+    return dateString.toLocaleString('pt-BR', {
         day: '2-digit',
         month: '2-digit',
         year: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-    }).replace(',', ' | ')
+    })
+}
+
+export function formatDate(dateString: string): string {
+    const today = new Date();
+    const date = new Date(dateString);
+
+    if (today.getDate() === date.getDate() && today.getMonth() === date.getMonth() && today.getFullYear() === date.getFullYear()) {
+        return `Today | ${getTime(date)}`;
+    } else if (today.getDate() - date.getDate() === 1) {
+        return `Yesterday | ${getTime(date)}`
+    } else {
+        return `${getDate(date)} | ${getTime((date))}`
+    }
 }
